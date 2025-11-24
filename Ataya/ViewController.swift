919730fcea976f1d15df63a43a54b38ff87e8f99
maramshubbar <202305590@student.polytechnic.bh>
2025-11-24 15:26:57 +0300

    Add files via upload

---
+++ Ataya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ataya
+//
+//  Created by Maram on 24/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

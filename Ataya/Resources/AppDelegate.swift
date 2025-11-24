3cf86869bea6a9ef41242eeaf2b093e6e22f1bab
maramshubbar <202305590@student.polytechnic.bh>
2025-11-24 17:59:54 +0300

    Uploading

--- Ataya/Resources/AppDelegate.swift
+++ Ataya/Resources/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,8 +14,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        FirebaseApp.configure()
+        
+        
         return true
     }
+    
+    /*
+    import UIKit
+     
+    import FirebaseCore
+    @UIApplicationMain
+     
+    class AppDelegate: UIResponder, UIApplicationDelegate {
+     
+     
+      var window: UIWindow?
+     
+     
+      func application(_ application: UIApplication,
+     
+        didFinishLaunchingWithOptions launchOptions:
+     
+          [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+     
+        FirebaseApp.configure()
+     
+     
+        return true
+     
+      }*/
+     
+    
+
 
     // MARK: UISceneSession Lifecycle
 

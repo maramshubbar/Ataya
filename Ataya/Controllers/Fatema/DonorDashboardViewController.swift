364f1494fcc69dea20304acb76cc9575978b14f5
Fatema Maitham <202304661@student.polytechnic.bh>
2025-11-25 16:49:40 +0300

    Donor dashboard inital

---
+++ Ataya/Controllers/Fatema/DonorDashboardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DonorDashboardViewController.swift
+//  Ataya
+//
+//  Created by Fatema Maitham on 25/11/2025.
+//
+
+import Foundation
+import UIKit
+
+class DonorDashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // This will run when the Donor Dashboard screen loads
+    }
+
+}

befb85591d18c259245ba9b4766f0a0af19fb893
Fatema Maitham <202304661@student.polytechnic.bh>
2025-11-26 00:23:59 +0300

    Shadow

--- Ataya/Controllers/Fatema/DonateViewController.swift
+++ Ataya/Controllers/Fatema/DonateViewController.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+@IBOutlet weak var cardView: UIView!
+
+
+override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Rounded corners
+    cardView.layer.cornerRadius = 20
+
+    // Shadow
+    cardView.layer.shadowColor = UIColor.black.cgColor
+    cardView.layer.shadowOpacity = 0.15
+    cardView.layer.shadowOffset = CGSize(width: 0, height: 6)
+    cardView.layer.shadowRadius = 12
+
+    // Required to show shadow
+    cardView.layer.masksToBounds = false
+}

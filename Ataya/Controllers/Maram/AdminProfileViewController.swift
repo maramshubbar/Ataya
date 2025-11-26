3c8bf6ba91cb2134acf2a65d7ee06c5f332c6220
maramshubbar <202305590@student.polytechnic.bh>
2025-11-26 14:39:57 +0300

    admin profile

--- Ataya/Controllers/Maram/AdminProfileViewController.swift
+++ Ataya/Controllers/Maram/AdminProfileViewController.swift
@@ -8,44 +8,86 @@
 import UIKit
 
 class AdminProfileViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
+        nameLabel.text = "Abdulla Yusuf"
         
-        profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
-        profileImageView.clipsToBounds = true
-        profileImageView.contentMode = .scaleAspectFill
-            
-            nameLabel.text = ""
-            roleLabel.text = ""
         
-
-
+        
+        
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        aboutMeButton.layer.cornerRadius = 8
+        aboutMeButton.layer.borderWidth = 1
+        aboutMeButton.layer.borderColor = UIColor.systemGray4.cgColor
+        aboutMeButton.layer.masksToBounds = false
+        
+        notificationButton.layer.cornerRadius = 8
+        notificationButton.layer.borderWidth = 1
+        notificationButton.layer.borderColor = UIColor.systemGray4.cgColor
+        notificationButton.layer.masksToBounds = false
+        
+        
+        logoutButton.layer.cornerRadius = 8
+        logoutButton.layer.borderWidth = 1
+        logoutButton.layer.borderColor = UIColor.systemGray4.cgColor
+        logoutButton.layer.masksToBounds = false
+        
+        makeProfileImageCircular()
+    }
     
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
     
-        
+    
     @IBOutlet weak var roleLabel: UILabel!
     
+    @IBOutlet weak var aboutMeButton: UIButton!
+    
+    @IBOutlet weak var notificationButton: UIButton!
+    
+    @IBOutlet weak var logoutButton: UIButton!
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func makeProfileImageCircular() {
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
+        profileImageView.clipsToBounds = true
+        profileImageView.contentMode = .scaleAspectFill
     }
-    */
+    
+    
+    
+    @IBAction func aboutMeButton(_ sender: UIButton) {
+    }
+    
     
    
-
+    @IBAction func aboutMeButtonTapped(_ sender: Any) {
+        if let aboutVC = storyboard?.instantiateViewController(withIdentifier: "AboutMeViewController") as? AboutMeViewController {
+            aboutVC.modalPresentationStyle = .fullScreen
+            present(aboutVC, animated: true)
+        }
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
+    }
+        
+    
 }

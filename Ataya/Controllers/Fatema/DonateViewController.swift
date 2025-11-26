0e99c4a810c6642e3b5ec4576aaf7fa019f6e4d0
Fatema Maitham <202304661@student.polytechnic.bh>
2025-11-26 09:41:50 +0300

    Shadow 1

--- Ataya/Controllers/Fatema/DonateViewController.swift
+++ Ataya/Controllers/Fatema/DonateViewController.swift
@@ -9,13 +9,23 @@ import UIKit
 
 class DonateViewController: UIViewController {
 
+    @IBOutlet weak var cardView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            
+            // Rounded corners
+            cardView.layer.cornerRadius = 20
+            
+            // Shadow
+            cardView.layer.shadowColor = UIColor.black.cgColor
+            cardView.layer.shadowOpacity = 0.15
+            cardView.layer.shadowOffset = CGSize(width: 0, height: 6)
+            cardView.layer.shadowRadius = 12
+            
+            // Required to show shadow
+            cardView.layer.masksToBounds = false
     }
-    
-
+        
     /*
     // MARK: - Navigation
 

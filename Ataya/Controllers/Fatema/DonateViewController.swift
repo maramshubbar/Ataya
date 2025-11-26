1aa0ba345a3631fc61330b3a137fd2fe3b85e667
maramshubbar <202305590@student.polytechnic.bh>
2025-11-26 09:24:55 +0300

    uload

--- Ataya/Controllers/Fatema/DonateViewController.swift
+++ Ataya/Controllers/Fatema/DonateViewController.swift
@@ -2,6 +2,28 @@
 //  DonateViewController.swift
 //  Ataya
 //
-//  Created by Fatema Maitham on 25/11/2025.
+//  Created by Maram on 26/11/2025.
 //
-import Foundation
+
+import UIKit
+
+class DonateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

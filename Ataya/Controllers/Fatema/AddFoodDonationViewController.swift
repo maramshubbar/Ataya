a5f07ce6f34308e06d4d8d8cca14eeb3e1a25f94
Fatema Maitham <202304661@student.polytechnic.bh>
2025-11-27 17:47:14 +0300

    -

---
+++ Ataya/Controllers/Fatema/AddFoodDonationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddFoodDonationViewController.swift
+//  Ataya
+//
+//  Created by Fatema Maitham on 27/11/2025.
+//
+
+import UIKit
+
+class AddFoodDonationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

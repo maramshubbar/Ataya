85a3d7c1b8dacee904ee9978639c1860fac29e9d
Fatema Maitham <202304661@student.polytechnic.bh>
2025-11-28 19:41:37 +0300

    upload photo

--- Ataya/Views/Storyboards/Fatema/Add Food Donation/UploadPhotosViewController.swift
+++ Ataya/Views/Storyboards/Fatema/Add Food Donation/UploadPhotosViewController.swift
@@ -9,12 +9,33 @@ import UIKit
 
 class UploadPhotosViewController: UIViewController {
 
+    @IBOutlet weak var uploadCardView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        uploadCardView.layoutIfNeeded()
+        
+        // Remove previous dashed layer so it doesn't duplicate
+        uploadCardView.layer.sublayers?.removeAll(where: { $0.name == "dashedBorder" })
+        
+        let dashed = CAShapeLayer()
+        dashed.name = "dashedBorder"
+        dashed.path = UIBezierPath(roundedRect: uploadCardView.bounds, cornerRadius: 12).cgPath
+        dashed.strokeColor = UIColor.systemGray3.cgColor
+        dashed.lineWidth = 1
+        dashed.lineDashPattern = [6, 4]
+        dashed.fillColor = UIColor.clear.cgColor
+        
+        uploadCardView.layer.addSublayer(dashed)
+    }
+
 
     /*
     // MARK: - Navigation

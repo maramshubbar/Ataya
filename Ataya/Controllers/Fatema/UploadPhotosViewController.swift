bd60fe8cec8cd477864acdba438638376190f79d
Fatema Maitham <202304661@student.polytechnic.bh>
2025-11-28 11:17:49 +0300

    upload photos

---
+++ Ataya/Controllers/Fatema/UploadPhotosViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UploadPhotosViewController.swift
+//  Ataya
+//
+//  Created by Fatema Maitham on 28/11/2025.
+//
+
+import UIKit
+
+class UploadPhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

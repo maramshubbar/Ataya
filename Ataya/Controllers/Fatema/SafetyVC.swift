feee1211cb9976e039a63dd60ee21d39f6518cec
Fatema Maitham <202304661@student.polytechnic.bh>
2025-11-29 16:18:20 +0300

    safety

---
+++ Ataya/Controllers/Fatema/SafetyVC.swift
@@ -0,0 +1,29 @@
+//
+//  SafetyVC.swift
+//  Ataya
+//
+//  Created by Fatema Maitham on 29/11/2025.
+//
+
+import UIKit
+
+class SafetyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

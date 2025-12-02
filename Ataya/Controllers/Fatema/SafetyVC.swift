135b98fb98c6dd9a1b5cc7f299ef5df1c53fdea7
Fatema Maitham <202304661@student.polytechnic.bh>
2025-12-02 14:00:56 +0300

    checkbox

--- Ataya/Controllers/Fatema/SafetyVC.swift
+++ Ataya/Controllers/Fatema/SafetyVC.swift
@@ -8,14 +8,43 @@
 import UIKit
 
 class SafetyVC: UIViewController {
-
+    @IBOutlet weak var checkboxButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Start with unchecked state
+        checkboxButton.isSelected = false
+        checkboxButton.setImage(UIImage(named: "checkbox_unchecked"), for: .normal)
+        checkboxButton.setImage(UIImage(named: "checkbox_checked"), for: .selected)
+        
+        // Configure button disabled at start
+        configureNextButton(isEnabled: false)
     }
     
-
+    @IBAction func checkboxTapped(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        
+        // Update Next button based on checkbox state
+        configureNextButton(isEnabled: sender.isSelected)
+    }
+    
+    private func configureNextButton(isEnabled: Bool) {
+        nextButton.isEnabled = isEnabled
+        
+        if isEnabled {
+            // Checked → Yellow button
+            nextButton.backgroundColor = UIColor(hex: "#FFD83F")
+            nextButton.setTitleColor(.white, for: .normal)
+        } else {
+            // Unchecked → Light grey button
+            nextButton.backgroundColor = UIColor(hex: "#E8E8E8")
+            nextButton.setTitleColor(.white, for: .disabled) // ALWAYS white text
+        }
+    }
+    
+}
     /*
     // MARK: - Navigation
 
@@ -26,4 +55,3 @@ class SafetyVC: UIViewController {
     }
     */
 
-}

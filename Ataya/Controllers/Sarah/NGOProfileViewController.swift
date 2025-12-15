3869fb943796c313ef69d9d63de6b7c368404514
Sarah <202304907@student.polytechnic.bh>
2025-12-15 14:25:29 +0300

    Edit profile

---
+++ Ataya/Controllers/Sarah/NGOProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NGOProfileViewController.swift
+//  Ataya
+//
+//  Created by BP-19-130-11 on 15/12/2025.
+//
+
+import UIKit
+
+class NGOProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

7964b5b8429581dcdbb498fbd9f4e33c7beeebce
maramshubbar <202305590@student.polytechnic.bh>
2025-12-18 20:02:13 +0300

    reward page

---
+++ Ataya/Controllers/RewardsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RewardsViewController.swift
+//  Ataya
+//
+//  Created by Maram on 18/12/2025.
+//
+
+import UIKit
+
+class RewardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}

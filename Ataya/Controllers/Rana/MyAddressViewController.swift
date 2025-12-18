cccbc604b6e042ed6162763ecb459f0a4206072c
BaderAl3li <202303330@student.polytechnic.bh>
2025-12-18 13:58:41 +0300

    updated

---
+++ Ataya/Controllers/Rana/MyAddressViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyAddressViewController.swift
+//  Ataya
+//
+//  Created by BP-36-224-16 on 18/12/2025.
+//
+
+import UIKit
+
+class MyAddressViewController: UIViewController {
+    
+    @IBOutlet weak var buttonContainer: UIView!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var myAddressButton: UIButton!
+    
+    @IBOutlet weak var ngoButton: UIButton!
+    
+    
+}

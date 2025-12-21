7ee7d16dd774714aa1cbc469c7bf8b3651fbf50c
RanaAlqameesh <202200728@student.polytechnic.bh>
2025-12-21 15:47:28 +0300

    update confirm

---
+++ Ataya/Controllers/Rana/MyAddressListTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyAddressListTableViewController.swift
+//  Ataya
+//
+//  Created by BP-36-215-01 on 21/12/2025.
+//
+
+import UIKit
+
+class MyAddressListTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

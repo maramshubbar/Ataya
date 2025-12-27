4d60748323c8b37a4309b78f3d50b61092f04117
Ruqaya <202200088@student.polytechnic.bh>
2025-12-27 15:53:57 +0300

    update

--- Ataya/Controllers/Ruqaya/ngoDashboard/ngoDashboardViewController.swift
+++ Ataya/Controllers/Ruqaya/ngoDashboard/ngoDashboardViewController.swift
@@ -7,14 +7,83 @@
 
 import UIKit
 
-class ngoDashboardViewController: UIViewController {
+class ngoDashboardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var assignedTableView: UITableView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var assignedTableHeightConstraint: NSLayoutConstraint!
+    
+    
+    struct AssignedPickupItem {
+        let title: String
+        let donor: String
+        let location: String
+        let status: String
+    }
+
+    private let assignedPickups: [AssignedPickupItem] = [
+        .init(title: "Canned Beans",
+              donor: "Abdulla Hasan (ID: D-17)",
+              location: "Hamad Town, Bahrain",
+              status: "Upcoming"),
+        .init(title: "Cooking Oil",
+              donor: "Ali AlArab (ID: D-61)",
+              location: "Zayed Town, Bahrain",
+              status: "Upcoming")
+    ]
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        assignedTableView.dataSource = self
+        assignedTableView.delegate = self
+        assignedTableView.isScrollEnabled = false
+        assignedTableView.separatorStyle = .none
+        assignedTableView.backgroundColor = .clear
+
+        assignedTableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: "cell"
+        )
+
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return assignedPickups.count
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
+                                                 for: indexPath)
+
+        let item = assignedPickups[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = """
+        \(item.title)
+        \(item.donor)
+        \(item.location)
+        \(item.status)
+        """
+
+        cell.selectionStyle = .none
+        return cell
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        assignedTableView.layoutIfNeeded()
+        assignedTableHeightConstraint.constant =
+            assignedTableView.contentSize.height
+    }
+
 
     /*
     // MARK: - Navigation

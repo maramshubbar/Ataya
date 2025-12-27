9c2f19e54bd68227ab6db35db6605361b6ff3f40
Ruqaya <202200088@student.polytechnic.bh>
2025-12-27 15:42:01 +0300

    update dash

---
+++ Ataya/Controllers/Ruqaya/ngoDashboard/ngoDashboardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ngoDashboardViewController.swift
+//  Ataya
+//
+//  Created by Ruqaya Habib on 27/12/2025.
+//
+
+import UIKit
+
+class ngoDashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

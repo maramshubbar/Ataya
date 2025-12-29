f1ea7c84f57d45aa5eb77ba5437ca5b0fc34e015
mohamed <202304907@student.polytechnic.bh>
2025-12-29 17:12:52 +0300

    update

--- Ataya/Controllers/Sarah/DonorProfileViewController.swift
+++ Ataya/Controllers/Sarah/DonorProfileViewController.swift
@@ -8,34 +8,45 @@
 import UIKit
 
 class DonorProfileViewController: UIViewController {
-
+    
     @IBOutlet weak var Aboutme: UIView!
     
     @IBOutlet weak var Notification: UIButton!
     
-    @IBOutlet weak var RecurringHisttory: UIButton!
+    @IBOutlet weak var RecurringHistory: UIButton!
     
-    @IBOutlet weak var Darkmode: UIButton!
+    @IBOutlet weak var darkModeSwitch: UISwitch!
     
     @IBOutlet weak var HelpSupport: UIButton!
     
+    @IBOutlet weak var modeLabel: UILabel!
     @IBOutlet weak var Logout: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let isDarkMode = UserDefaults.standard.bool(forKey: "isDarkModeEnabled")
+        darkModeSwitch.isOn = isDarkMode
+        overrideUserInterfaceStyle = isDarkMode ? .dark : .light
+        modeLabel.text = isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"
     }
     
+    
+    
+    
+    @IBAction func darkModeToggled(_ sender: UISwitch) {
+        let isDarkMode = sender.isOn
+        overrideUserInterfaceStyle = isDarkMode ? .dark : .light
+        UserDefaults.standard.set(isDarkMode, forKey: "isDarkModeEnabled")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Update label to show the *next* mode
+        modeLabel.text = isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"
     }
-    */
 
+    
+    
+    
+    
 }
+
+

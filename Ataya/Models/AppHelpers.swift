f99a0d03afe3990b006b568dd53a3861aa5db753
Ruqaya <202200088@student.polytechnic.bh>
2026-01-01 17:25:15 +0300

    Revert "Merge branch 'ngoDashboard'"
    This reverts commit c6257edebd9fdbc62cb5c2479bf687e614cae033, reversing
    changes made to e9b61a0effb40da42c9c4aee73691bb5636e5956.

---
+++ Ataya/Models/AppHelpers.swift
@@ -0,0 +1,33 @@
+//
+//  AppHelpers.swift
+//  Ataya
+//
+//  Created by Maram on 24/12/2025.
+//
+
+import UIKit
+
+extension String {
+    var trimmed: String { trimmingCharacters(in: .whitespacesAndNewlines) }
+}
+
+extension Date {
+    var formattedShort: String {
+        let f = DateFormatter()
+        f.dateFormat = "MMM d yyyy"
+        return f.string(from: self)
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String) {
+        var h = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if h.hasPrefix("#") { h.removeFirst() }
+        var rgb: UInt64 = 0
+        Scanner(string: h).scanHexInt64(&rgb)
+        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255
+        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255
+        let b = CGFloat(rgb & 0x0000FF) / 255
+        self.init(red: r, green: g, blue: b, alpha: 1)
+    }
+}

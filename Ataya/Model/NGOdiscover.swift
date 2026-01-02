f525b22d1966d8ca0645e22a6d7009ea5b4c047d
Sarah <202304907@student.polytechnic.bh>
2026-01-02 19:30:27 +0300

    update

--- Ataya/Model/NGOdiscover.swift
+++ Ataya/Model/NGOdiscover.swift
@@ -18,4 +18,4 @@ struct NGOdiscover {
     let imageName: String 
 }
 
-/*init?(document: [String: Any]) { guard let name = document["name"] as? String, let category = document["category"] as? String, let email = document["email"] as? String, let location = document["location"] as? String, let rating = document["rating"] as? Double, let impact = document["impact"] as? Int, let mission = document["mission"] as? String, let activities = document["activities"] as? [String], let successStory = document["successStory"] as? String else { return nil } self.name = name self.category = category self.email = email self.location = location self.rating = rating self.impact = impact self.mission = mission self.activities = activities self.successStory = successStory }*/
+
